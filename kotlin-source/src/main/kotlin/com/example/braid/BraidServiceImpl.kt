7b534c059b4e5c7fa3108ea7a9acff0d3d48e1db
Pradeep Vishwakarma <pradeep.vishwakarma@cennest.com>
2019-01-09 18:10:50 +0530

    fixed the issue "Was expecting to find transaction set on current strand"

--- kotlin-source/src/main/kotlin/com/example/braid/BraidServiceImpl.kt
+++ kotlin-source/src/main/kotlin/com/example/braid/BraidServiceImpl.kt
@@ -1,5 +1,6 @@
 package com.example.braid
 
+import com.example.db.MyDbService
 import com.example.flow.ExampleFlow
 import com.example.state.IOUState
 import io.bluebank.braid.core.async.all
@@ -10,14 +11,19 @@ import io.vertx.core.Future
 import io.vertx.core.Vertx
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.node.AppServiceHub
-import net.corda.core.node.services.Vault
-import net.corda.core.node.services.queryBy
 import net.corda.core.node.services.vault.PageSpecification
 import net.corda.core.node.services.vault.QueryCriteria
 import net.corda.core.serialization.SingletonSerializeAsToken
 import net.corda.core.utilities.loggerFor
+import net.corda.nodeapi.internal.persistence.contextDatabase
+import net.corda.nodeapi.internal.persistence.contextTransaction
+import net.corda.nodeapi.internal.persistence.wrapWithDatabaseTransaction
 import org.slf4j.Logger
+import rx.BackpressureOverflow
 import rx.Observable
+import rx.Observer
+import rx.internal.operators.OperatorReplay.observeOn
+import rx.observables.SyncOnSubscribe
 import rx.schedulers.Schedulers
 
 
@@ -52,7 +58,9 @@ class BraidServiceImpl(private val serviceHub: AppServiceHub, private val vertx:
         val results = serviceHub.vaultService.trackBy(contractStateType = IOUState::class.java, criteria = criteria, paging = pageSpec)
         // we do this as a single subscribable flow - no need to subscribe within a subscription
         return results.updates
+            //.onBackpressureBuffer(1, () -> { }, BackpressureOverflow.ON_OVERFLOW_DROP_OLDEST)
             .observeOn(Schedulers.computation()) // this was the key technique to avoid jamming up corda's hibernate transactions
+            .wrapWithDatabaseTransaction()
             .map { // for each event, recompute the total
                 calculateTotalRecords()
             }
@@ -63,8 +71,33 @@ class BraidServiceImpl(private val serviceHub: AppServiceHub, private val vertx:
     }
 
     private fun calculateTotalRecords(): Long {
+
+        val dbService = serviceHub.cordaService(MyDbService::class.java)
+        val count =  dbService.getTotalRecords()?:0 //contextDatabase.transaction { dbService.getTotalRecords()?:0 }
+        return count
+
+        /*
         val criteria: QueryCriteria.LinearStateQueryCriteria = QueryCriteria.LinearStateQueryCriteria(status = Vault.StateStatus.UNCONSUMED)
         val results = serviceHub.vaultService.queryBy<IOUState>(criteria = criteria, paging = PageSpecification(1, 1))
         return results.totalStatesAvailable
+        */
+    }
+}
+
+
+open class CustomSyncOnSubscriber : SyncOnSubscribe<Long, Long>() {
+
+
+    override fun generateState(): Long = 0
+
+    override fun next(counter: Long, observer: Observer<in Long>): Long {
+
+
+
+        observer.onNext(counter)
+        return counter + 1
+    }
+    override fun onUnsubscribe(state: Long) {
+
     }
 }
